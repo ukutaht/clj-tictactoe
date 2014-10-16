06a588b3d95736232a31fcfa5b0cb05f67175252
Uku Taht <uku.taht@gmail.com>
2014-10-16 16:02:00 +0100

    formatting

--- spec/tictactoe/io_spec.clj
+++ spec/tictactoe/io_spec.clj
@@ -8,8 +8,8 @@
 (describe "command line io"
   (it "displays the board in correct format"
     (with-redefs [io/clear-screen #()]
-      (let [board [x-mark 1 2 3 4 5 6 7 o-mark]
-            output      (with-out-str ((command-line-io :show-board) board))]
+      (let [board   [x-mark 1 2 3 4 5 6 7 o-mark]
+            output  (with-out-str ((command-line-io :show-board) board))]
         (should= "X | 2 | 3\n4 | 5 | 6\n7 | 8 | O\n" output))))
 
   (it "presents the winner X"
@@ -18,5 +18,4 @@
 
   (it "presents the winner O"
     (let [output (with-out-str ((command-line-io :present-winner) o-wins-board))]
-      (should= "O wins\n" output)))
-  )
+      (should= "O wins\n" output))))
